aa412a7c9bb002ede9e3a5c66b40d06f87bd414b
AdrianK47 <114188316+AdrianK47@users.noreply.github.com>
2024-09-09 20:33:06 -0400

    hehehehe

--- SumofCubes.java
+++ SumofCubes.java
@@ -3,19 +3,23 @@ import java.util.*;
 public class SumofCubes{
     public static void main(String args[]){
         int userNumber;
+        double sum = 0;
 
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter a number: ");
         userNumber = input.nextInt();
 
-        String userString = String.valueOf(userNumber);
-        int size = userString.length();
+        int original = userNumber;
 
-        for(int i = 0; i < size; i++){
-            int sum = (((userNumber / 10) / 10) / 10);
-            System.out.println(sum);
+        do{
+            int n = userNumber % 10;
+            sum = sum + Math.pow(n,3);
+            userNumber = userNumber / 10;
         }
+        while (userNumber != 0);
+        System.out.println("The sum of the cubes of " + original + " is " + sum);
+
 
     }
 }
